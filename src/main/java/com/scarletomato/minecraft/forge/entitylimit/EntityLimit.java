00ecc23bdbf8a409c3c7638c720a8e1c03bf8cf9
Mike Yarbrough <Michael.Yarbrough@aimco.com>
2017-02-09 12:00:17 -0500

    added TPS counter

--- src/main/java/com/scarletomato/minecraft/forge/entitylimit/EntityLimit.java
+++ src/main/java/com/scarletomato/minecraft/forge/entitylimit/EntityLimit.java
@@ -6,10 +6,12 @@ import jline.internal.Log;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.init.Blocks;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.EntityRegistry.EntityRegistration;
 
@@ -21,6 +23,13 @@ public class EntityLimit
 {
     public static final String MODID = "entitylimit";
     public static final String VERSION = "1.0";
+    public Configuration config;
+    
+    @EventHandler
+    public void init(FMLPreInitializationEvent event)
+    {
+    	config = new Configuration(event.getSuggestedConfigurationFile());
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
--- src/main/java/com/scarletomato/minecraft/forge/entitylimit/SpawnEntityListener.java
+++ src/main/java/com/scarletomato/minecraft/forge/entitylimit/SpawnEntityListener.java
@@ -2,21 +2,29 @@ package com.scarletomato.minecraft.forge.entitylimit;
 
 import jline.internal.Log;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.EntityEvent;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent.CheckSpawn;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteractSpecific;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ServerTickEvent;
 
 public class SpawnEntityListener {
+	/**
+	 * How many old averages to remember
+	 */
+	private static final int WEIGHT = 10;
+	/**
+	 * How many ticks to count before recalculating the TPS
+	 */
+	private static final int COUNT = 50;
+	
+	long startTime = System.currentTimeMillis();
+	int ticks = 0;
+	double avgTps;
+	int avgWeight = 0;
 	
 	@SubscribeEvent
 	public void onEvent(CheckSpawn event) {
@@ -31,6 +39,39 @@ public class SpawnEntityListener {
 		Entity l = event.getTarget();
 		p.addChatMessage(new TextComponentString(l.getName()));
 		p.addChatMessage(new TextComponentString(l.getClass().toString()));
+	}
+	
+	@SubscribeEvent
+	public void onEvent(ServerTickEvent event) {
+		ticks++;
+		if(ticks >= COUNT) {
+			//running avg = (nowCalc + oldAvg*weight)/(weight+1)
+			avgTps = ((ticks*1000.0)/(System.currentTimeMillis() - startTime) + avgTps*avgWeight)/(avgWeight+1);
+			Log.info("current avg = " + avgTps);
+			
+			// restart count
+			startTime = System.currentTimeMillis();
+			ticks = 0;
+			if(0==avgWeight) {
+				avgWeight = WEIGHT;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		int simulatedTPS = 40;
+		SpawnEntityListener s = new SpawnEntityListener();
+		while(true) {
+			s.onEvent((ServerTickEvent)null);
+			try {
+				// 1000/TPS = milliseconds per tick
+				Thread.sleep(1000/simulatedTPS);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 //	@SubscribeEvent
